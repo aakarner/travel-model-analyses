ffe4c3e59b87d6372a82be0080908793f3477b14
aakarner <aakarner@ucdavis.edu>
2013-05-26 07:34:29 -0700

    added header

--- scripts/01 - generate travel data.R
+++ scripts/01 - generate travel data.R
@@ -1,16 +1,19 @@
-# setwd("E:/github repos/travel-data-analysis/data")
+# ----------------------------------
+# This script creates imagined distance skims for a 50 zone 
+# network with five possible modes.
+# 
+# The resultant csv files are already stored in the 'data'
+# directory on GitHub, so do not need to be recreated.
+# ----------------------------------
 
 # Create random distance skims
-
-# col 1 = origin
-# col 2 = destination
-# remaining columns = modes
-										
 dist <- rlnorm(2500, 3.0, 0.2)
 
+# Create a data frame to store skims. Skims for non-
+# drive alone are multiples of drive alone distance
 dist.skims.am <- data.frame(
-	orig = rep(1:50,50), # origins
-	dest = as.vector(sapply(1:50, function(x) rep(x,50))), # destinations
+	orig = rep(1:50,50),
+	dest = as.vector(sapply(1:50, function(x) rep(x,50))),
 	sov = dist, # sov
 	hov2 = dist * 1.1, # hov2
 	hov3 = dist * 1.15, # hov3
@@ -20,8 +23,7 @@ dist.skims.am <- data.frame(
 # Assume the same AM and PM distance skims
 dist.skims.pm <- dist.skims.am
 
-write.csv(SJV.sampl, "sample_trip table.csv", row.names = FALSE)
-write.csv(dist.skims.am, "dist_skims_am.csv", row.names = FALSE)
-write.csv(dist.skims.pm, "dist_skims_pm.csv", row.names = FALSE)
+write.csv(dist.skims.am, "data/dist_skims_am.csv", row.names = FALSE)
+write.csv(dist.skims.pm, "data/dist_skims_pm.csv", row.names = FALSE)
 
 # End data prep
